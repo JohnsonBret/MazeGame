bd0b9e9b6f60894b1787127eb28e734f6fab99b1
Bret Main <bretaudio@gmail.com>
2018-06-05 21:54:41 -0700

    Added alternate Launcher Script

---
+++ Assets/LauchBulletAlternate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauchBulletAlternate : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject bullet;
+			bullet = Instantiate (projectile, transform.position, transform.rotation);
+			Rigidbody bulletBody = bullet.GetComponent<Rigidbody> ();
+			bulletBody.velocity = Camera.main.transform.forward * speed;
+
+		}
+		
+	}
+}
